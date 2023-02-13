3dda41e22df89c20059c2c7adb75b174ab25564b
Ahmed Hamza <ahmd7mza@gmail.com>
2023-02-13 17:39:32 +0200

    Build the structure

---
+++ src/app/games/games.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllGamesComponent } from './components/all-games/all-games.component';
+import { GameDetailsComponent } from './components/game-details/game-details.component';
+import { AsideComponent } from './components/aside/aside.component';
+
+
+
+@NgModule({
+  declarations: [
+    AllGamesComponent,
+    GameDetailsComponent,
+    AsideComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GamesModule { }
